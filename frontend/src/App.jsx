8a363a75f5410332bbd54b9d5df331dbc3dbcec3
Parth Choudhary <parthchoudhary4372@gmail.com>
2026-05-10 08:36:40 +0530

    removing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
               Sign Up
             </h1>
 
-            <div className="w-full flex-1 mt-8 pb-8 mt-14">
+            <div className="w-full flex-1 mt-8 pb-8">
               {/* Social Buttons */}
               <div className="flex flex-col items-center">
                 {/* Google */}
