a20e0033374b1776e85d24d37f5c2a32d3f01faf
Parth Choudhary <parthchoudhary4372@gmail.com>
2026-05-10 08:58:24 +0530

    creating backend folders

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
 
             <div className="w-full flex-1 mt-8 pb-8">
               {/* Social Buttons */}
-              <div className="flex flex-col items-center">
+              <div cla0ssName="flex flex-col items-center">
                 {/* Google */}
                 <button className="w-full max-w-xs font-bold shadow-sm rounded-lg py-3 bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out hover:shadow-md focus:outline-none">
                   <div className="bg-white p-2 rounded-full">
